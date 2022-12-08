9fbbc29f682fd6a31aa8ace424a9a27244eb9887
António Cunha <aj.20.cunha@gmail.com>
2022-12-08 17:54:58 +0000

    🎉 Setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['jest'],
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
